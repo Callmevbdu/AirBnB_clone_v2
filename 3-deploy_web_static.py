49887fb1aacb229ff54cc6dcb8a4f152d5d2898d
Abdellatif <abdelt.2012@gmail.com>
2024-03-08 04:00:09 +0100

    Done.

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,61 +11,68 @@ env.hosts = ["54.221.13.125", "52.91.117.179"]
 
 
 def do_pack():
-    """ Creating an archive from web_static folder """
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    """
+    Compresses the web_static folder into a .tgz archive.
+    """
+
+    now = datetime.now()
+    form = 'web_static_' + now.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+
+    local('mkdir -p versions')
+
+    tgz = local("tar -czvf versions/{} web_static/".format(form))
+
+    if tgz is not None:
+        return form
+    else:
         return None
-    return file
 
 
 def do_deploy(archive_path):
-    """
-    Passing the archive to the web server
+
+    """Distributes an archive to a web server.
 
     Args:
-        archive_path (str): The archive's path
-    Return:
-        Error - False, otherwise - True
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        Error occurs - False.
+        Otherwise - True.
     """
-
     if os.path.isfile(archive_path) is False:
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    fName = archive_path.split("/")[-1]
+    file = fName.split(".")[0]
+    fDir = "/data/web_static/releases/"
+    current = "/data/web_static/current"
+    if put(archive_path, "/tmp/{}".format(fName)).failed is True:
+        return False
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    if run("rm -rf {}{}/".format(fDir, file)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+
+    if run("mkdir -p {}{}/".format(fDir, file)).failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+
+    if run("tar -xzf /tmp/{} -C {}{}/".
+            format(fName, fDir, file)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+
+    if run("rm /tmp/{}".format(fName)).failed is True:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+
+    if run("mv {}{}/web_static/* {}{}/"
+            .format(fDir, file, fDir, file)).failed is True:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+
+    if run('rm -rf {}{}/web_static'.format(fDir, file)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+
+    if run('rm -rf /data/web_static/current').failed is True:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+
+    if run('ln -s {}{}/ {}'.format(fDir, file, current)).failed is True:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
+
     return True
 
 
