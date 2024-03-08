662d9c4a0a372b3c1223b5c923c16e4df3b3f42f
Abdellatif <abdelt.2012@gmail.com>
2024-03-08 02:00:23 +0100

    Done.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,7 +15,6 @@ def do_deploy(archive_path):
     """
 
     if not exists(archive_path):
-        print(f"Archive not found: {archive_path}")
         return False
     try:
         fileName = archive_path.split("/")[-1]
@@ -26,12 +25,11 @@ def do_deploy(archive_path):
         run("mkdir -p {}{}/".format(releasedir, ext))
         run("tar -xzf /tmp/{} -C {}{}/".format(fileName, releasedir, ext))
 
-        run("rm /tmp/{fileName}")
+        run("rm /tmp/{}".format(fileName))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(releasedir, ext))
         run('rm -rf {}{}/web_static'.format(releasedir, ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(releasedir, ext))
         return True
-    except Exception as e:
-        print(f"An error occurred during deployment: {e}")
+    except:
         return False
