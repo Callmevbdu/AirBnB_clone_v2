5299587bf26aa36b6d78f551bb4c4322e30b2303
Abdellatif <abdelt.2012@gmail.com>
2024-03-08 03:13:27 +0100

    Done.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ a Fabric script (based on the file 1-pack_web_static.py) that distributes an
 archive to your web servers, using the function do_deploy
 """
 from fabric.api import put, run, env
-from os.path import exists
+import os.path
 
 env.hosts = ['54.221.13.125', '52.91.117.179']
 
@@ -13,24 +13,24 @@ def do_deploy(archive_path):
     """
     Deploys the archive to web servers.
     """
-
-    if not exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
+    fName = archive_path.split("/")[-1]
+    file = fName.split(".")[0]
+    fDir = "/data/web_static/releases/"
     try:
-        fileName = archive_path.split("/")[-1]
-        ext = fileName.split(".")[0]
-        releasedir = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
+        put(archive_path, "/tmp/{}".format(fName))
 
-        run("mkdir -p {}{}/".format(releasedir, ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(fileName, releasedir, ext))
-
-        run("rm /tmp/{}".format(fileName))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(releasedir, ext))
-        run('rm -rf {}{}/web_static'.format(releasedir, ext))
+        run("rm -rf {}{}/".format(fDir, file))
+        run("mkdir -p {}{}/".format(fDir, file))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fName, fDir, file))
+        run("rm /tmp/{}".format(fName))
+        run("mv /data/web_static/releases/{}/web_static/* "
+                "/data/web_static/releases/{}/".format(file, file))
+        run('rm -rf {}{}/web_static'.format(fDir, file))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(releasedir, ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(fDir, file))
         return True
     except Exception as e:
+        print e
         return False
-
