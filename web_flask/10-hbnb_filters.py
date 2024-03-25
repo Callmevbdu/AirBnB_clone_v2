16de1f065dc2877444ad40cbe803725bcb86b5e6
Abdellatif <abdelt.2012@gmail.com>
2024-03-25 02:58:08 +0000

    Done.

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
+"""
+A script that starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /hbnb_filters: HBnB HTML filters page.
 """
+
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -22,10 +22,10 @@ def hbnb_filters():
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(exception):
+    """Removes the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
+"""
+A script that starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /states: HTML page with a list of all State objects.
     /states/<id>: HTML page displaying the given state with <id>.
 """
+
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
 def states():
-    """Displays an HTML page with a list of all States.
-
-    States are sorted by name.
+    """
+    Displays an HTML page with a list of all States.
     """
     states = storage.all("State")
     return render_template("9-states.html", state=states)
@@ -25,7 +24,7 @@ def states():
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
+    """Displays an HTML page with info about <id>"""
     for state in storage.all("State").values():
         if state.id == id:
             return render_template("9-states.html", state=state)
@@ -34,9 +33,9 @@ def states_id(id):
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """Removes the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
