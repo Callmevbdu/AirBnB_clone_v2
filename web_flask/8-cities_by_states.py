0c78167d6a5b2cb25d398e97ac721e4bd1355281
Abdellatif <abdelt.2012@gmail.com>
2024-03-25 02:54:50 +0000

    Done.

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
+"""
+A script that starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /cities_by_states: HTML page with a list of all states and related cities.
 """
+
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
+    """
+    Displays an HTML page with a list of all states and related cities.
     States/cities are sorted by name.
     """
     states = storage.all("State")
@@ -24,9 +24,9 @@ def cities_by_states():
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """Removes the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
